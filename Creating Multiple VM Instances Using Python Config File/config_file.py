f632da179f0ab904d06bccd3161a79d707e2f4ff
MedhaRudra <37952639+MedhaRudra@users.noreply.github.com>
2020-03-08 16:59:34 +0530

    Update config_file.py

--- Creating Multiple VM Instances Using Python Config File/config_file.py
+++ Creating Multiple VM Instances Using Python Config File/config_file.py
@@ -2,6 +2,7 @@ class MyConfiguration():
 
     #setting the configuration of vm instance
     def setconfig():
+	
         config = {
             "machineType": "zones/us-central1-a/machineTypes/n1-standard-1",
             "disks":
@@ -13,13 +14,17 @@ class MyConfiguration():
             ],
             "networkInterfaces": [{"network": "global/networks/default"}]
         }
-        return config
+        return config, self.no_of_vm, self.project_name, self.zone_name
     
-    def myinput():
-        
-        project_name="qwiklabs-gcp-00-832737805b5c"
+    def __init__(self,project_name,zone_name,no_of_vm):
+	self.project_name=project_name
+	self.zone_name=zone_name
+	self.no_of_vm=no_of_vm
+	
+def main():
+	project_name="qwiklabs-gcp-00-832737805b5c"
         zone_name="us-central1-a"
         no_of_vm="2"
-        
-        return no_of_vm, project_name, zone_name
+	conf= MyConfiguration(project_name,zone_name,no_of_vm)
+	conf.setconfig()
 		
